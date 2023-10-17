9dca1cea84927e64f6e53d847d7eac3147fad3b4
Daniel <danielcarrilmiranda@alumnos.tuniversformacion.es>
2023-10-17 12:20:55 +0200

    Creo la carpeta con todos los proyectos de DWES

---
+++ PHP/dwes1_1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello World</title>
+</head>
+<body>
+    <h1>
+        <?php
+            echo "Hello World";
+        ?>
+    </h1>
+    <p>
+        <?php 
+            print "Pinta Bien";
+        ?>
+    </p>
+</body>
+</html>
